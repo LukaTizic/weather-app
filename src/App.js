effa9065df946340a24e7e0b8a75cdfcbeb66451
aluca94@gmail.com <LukaTizic>
2022-11-24 13:35:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaSearch, FaTemperatureHigh, FaTemperatureLow } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
+// const API_KEY = 895284fb2d2c50a520ea537456963d9c;
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const getWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=52027db2366aa0d36c54d31325690e4e`
+      );
+      setData(response.data);
+      console.log(response.data);
+      setLocation("");
+    } catch (error) {
+      alert("Please provide value");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='header'>
+        <h1 className='title'>Weather App</h1>
+      </div>
+      <div className='input-field'>
+        <input
+          value={location}
+          type='text'
+          placeholder='Please enter city'
+          onChange={handleChange}
+        />
+        <button className='btn' onClick={getWeather}>
+          <FaSearch />
+        </button>
+      </div>
+      <div className='container'>
+        <div>
+          <h1 className='city-name'>{data.name}</h1>
+          {data.sys ? <h2 className='country'>{data.sys.country}</h2> : null}
+        </div>
+        <div className='grid-container'>
+          <div className='grid-field'>
+            <p className='temp'>
+              Current temperature:
+              <br />
+              {data.main ? (
+                <span>{Math.trunc(data.main.temp - 273.15)}</span>
+              ) : null}
+              <FaTemperatureHigh />C
+            </p>
+
+            <p className='temp-feels'>
+              Feels like:
+              <br />
+              {data.main ? (
+                <span>{Math.trunc(data.main.feels_like - 273.15)}</span>
+              ) : null}
+              <FaTemperatureHigh />C
+            </p>
+            <p className='min-temp'>
+              Min temperature: <br />
+              {data.main ? (
+                <span>{Math.trunc(data.main.temp_min - 273.15)}</span>
+              ) : null}
+              <FaTemperatureLow />C
+            </p>
+            <p className='max-temp'>
+              Max temperature:
+              <br />
+              {data.main ? (
+                <span>{Math.trunc(data.main.temp_max - 273.15)}</span>
+              ) : null}
+              <FaTemperatureHigh />C
+            </p>
+          </div>
+          <div className='grid-field'>
+            <h1>Current weather</h1>
+            {data.weather ? (
+              <p className='current-weather'>{data.weather[0].main}</p>
+            ) : null}
+          </div>
+          <div className='grid-field'>
+            <p className='pressure'>
+              Pressure: <br />
+              {data.main ? <span>{data.main.humidity}</span> : null}hPa
+            </p>
+            <p>
+              Humidity:
+              <br />
+              {data.main ? <span>{data.main.humidity}</span> : null}%
+            </p>
+            <p>
+              Visibility:
+              <br />
+              {data.visibility ? <span>{data.visibility / 1000}</span> : null}km
+            </p>
+            <p>
+              Clouds: <br />
+              {data.clouds ? <span>{data.clouds.all}</span> : null}%
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/mockdata.js
@@ -0,0 +1,43 @@
+export default {
+  coord: {
+    lon: 20.4651,
+    lat: 44.804,
+  },
+  weather: [
+    {
+      id: 701,
+      main: "Mist",
+      description: "mist",
+      icon: "50d",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 278.91,
+    feels_like: 276.18,
+    temp_min: 278.84,
+    temp_max: 281.06,
+    pressure: 1007,
+    humidity: 99,
+  },
+  visibility: 2200,
+  wind: {
+    speed: 3.6,
+    deg: 310,
+  },
+  clouds: {
+    all: 100,
+  },
+  dt: 1668942332,
+  sys: {
+    type: 2,
+    id: 2037836,
+    country: "RS",
+    sunrise: 1668922903,
+    sunset: 1668956762,
+  },
+  timezone: 3600,
+  id: 792680,
+  name: "Belgrade",
+  cod: 200,
+};
